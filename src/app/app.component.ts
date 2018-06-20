d17d746aefb6d599f0c6040be4b11d2db7836e86
Eduardo <ibredu@gmail.com>
2018-06-20 16:52:31 -0500

    New repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nombre: string;
+  edad: number;
+  miArreglo = [
+    {nombre: 'Eduardo', edad: 28},
+    {nombre: 'Víctor', edad: 25},
+    {nombre: 'Anita', edad: 17},
+    {nombre: 'Juanito', edad: 12}
+  ];
+  persona1 = {nombre: 'Juanito', edad: 22};
+  persona2 = {nombre: 'Anita', edad: 17};
+  usuarios = [
+    {nombre: 'Anita', cal: 72},
+    {nombre: 'Juanito', cal: 68}
+  ];
+  nombreUsuario: string;
+  calUsuario: number;
+  currentUser = {
+    nombre: null,
+    cal: null
+  };
+  seleccionarUsuario(usuario) {
+    this.currentUser = usuario;
+  }
+  eliminarUsuario(i) {
+    this.usuarios.splice(i, 1);
+  }
+  agregarUsuario() {
+    this.usuarios.push(this.currentUser);
+    this.nombreUsuario = null;
+    this.calUsuario = null;
+  }
+  imprimirArreglo() {
+    console.log(this.miArreglo);
+    this.miArreglo.forEach( (elemento) => {
+      console.log(elemento);
+    } );
+  }
+  saludar() {
+    alert('Hola ' + this.nombre);
+  }
+  mostrarEdad() {
+    console.log(this.edad);
+  }
 }
+//app.module.ts -> app.component.ts -> app.component.html
+//Módulos -> Componentes
